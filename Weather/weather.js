7fa94bbcc25cf355e8a5595ae63f92db3f68feac
Lalit Kumar Badhotiya <lalitkumar.ac106@gmail.com>
2025-02-03 18:02:57 +0530

    Update weather.js

--- Weather/weather.js
+++ Weather/weather.js
@@ -1,4 +1,4 @@
-const apiKey = '8d6454a89dff871786a0307b0dbebbee'
+const apiKey = '8d6454a89dff871786a0307b0dbebbee' //Api key
 async function fetchWeatherData(city) {
     try {const response = await fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
