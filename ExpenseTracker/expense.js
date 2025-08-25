746fdff8f4e0ad65cdefb98a889a76d339b36b79
Lalit Kumar Badhotiya <lalitkumar.ac106@gmail.com>
2025-08-25 19:34:09 +0530

    Update expense.js

--- ExpenseTracker/expense.js
+++ ExpenseTracker/expense.js
@@ -11,14 +11,7 @@ const balance = document.getElementById(
   const form = document.getElementById("form");
   const text = document.getElementById("text");
   const amount = document.getElementById("amount");
-  // const dummyTransactions = [
-  //   { id: 1, text: "Flower", amount: -20 },
-  //   { id: 2, text: "Salary", amount: 300 },
-  //   { id: 3, text: "Book", amount: -10 },
-  //   { id: 4, text: "Camera", amount: 150 },
-  // ];
-  
-  // let transactions = dummyTransactions;
+ 
   
   //last 
   const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
